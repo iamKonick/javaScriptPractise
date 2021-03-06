6c9297fc8f5ee04d267836fc198f55032f230086
Md. Efthakhar Ul Alam <67385172+iamKonick@users.noreply.github.com>
2021-03-06 15:03:24 +0600

    Create javaScriptPractise.js
    Practice codes

---
+++ javaScriptPractise.js
@@ -0,0 +1,34 @@
+var ageOfMonkey = 12;
+var productDescription = "This is a laptop that is powered by M3 chip with 4 GB graphics card inbuilt";
+
+console.log(productDescription.toUpperCase());
+
+console.log(productDescription.indexOf("laptop"));
+
+console.log(productDescription.split(' '));
+
+
+var number1 = 25.5;
+var number2 = '15.5';
+var number3 = 21.5
+var number4 = 100;
+var number5 = 0.1;
+var number6 = 0.2;
+//console.log(number1 + (+number2));
+
+number2 = parseFloat(number2);
+number3 = parseInt(number3);
+number4 = ''+number4;
+
+console.log(number3);
+
+console.log(typeof number4);
+
+var total = number5 + number6;
+total = total.toFixed(2)
+console.log(total);
+
+
+
+
+
